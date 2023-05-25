26ed6c2dd8685f87d330b32200f2b51a1f5a9ea1
Max <max.akh.on@gmail.com>
2023-05-25 09:51:06 +0300

    number of films prog

--- js/js.js
+++ js/js.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Hello');
+const numberOfFilms = +prompt('Скільки фільмів ви подивились?','');
+
+const personalMovieDB = {
+	count : numberOfFilms,
+	movies :{},
+	actors : {},
+	genres : [],
+	privat : false
+};
+
+
+const a = prompt('Ваш останній фільм?',''),
+		b = prompt('На скільки ви його оцінюєте ?',''),
+		c = prompt('Ваш останній фільм?',''),
+		d = prompt('На скільки ви його оцінюєте ?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
